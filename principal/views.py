89d7a445b88665cce6eccc9e465a534efc577306
Diego Guedes <diegogsousa@yahoo.com>
2026-08-09 15:46:56 -0300

    Make PTM identity explicit in the public home

--- principal/views.py
+++ principal/views.py
@@ -1,13 +1,14 @@
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
+from django.shortcuts import render
 
 # Create your views here.
 
 
 def index(request):
-    return HttpResponse("PTM app is running")
+    return render(request, "ptm.html")
 
 
 @login_required
 def ptmprincipal(request):
-    return HttpResponse("PTM protected area")
+    return render(request, "ptm.html")
